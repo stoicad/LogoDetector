c41fac103b9909f41ab5bebdd5642efa1ddb8a87
Dragos Stoica <dragos.stoica@ness.com>
2019-06-24 16:06:23 +0300

    Pilot

---
+++ LogoDetector/LogoDetector/LogoDetector.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.CurrentActivity;
+using Plugin.Media;
+using Plugin.Permissions;
+
+namespace LogoDetector.Droid
+{
+    [Activity(Label = "LogoDetector", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+
+
+            await CrossMedia.Current.Initialize();
+
+
+            base.OnCreate(savedInstanceState);
+
+            CrossCurrentActivity.Current.Activity = this;
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTExMzk2QDMxMzcyZTMxMmUzMEhaM0lZVkkrTUM5UzVNLzZPa1FTY3ZUSlVOaTc2YSs4SmtjUU5XN3lJcFU9");
+            await CrossPermissions.Current.RequestPermissionsAsync(Plugin.Permissions.Abstractions.Permission.Camera);
+            await CrossPermissions.Current.RequestPermissionsAsync(Plugin.Permissions.Abstractions.Permission.Storage);
+            LoadApplication(new App());
+        }
+    }
+}
